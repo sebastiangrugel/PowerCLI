39f31418156b26915612343405fcd7f242877d5f
GRUGEL <sebastian.grugel@atos.net>
2018-04-05 21:03:35 +0200

    first commit

---
+++ PowerCLI_Restart_VMGuest.ps1
@@ -0,0 +1,9 @@
+$servers = Get-Content d:\a545632\vmr2.txt
+foreach ($s in $servers) {
+Write-Host Initiating Reboot for $s
+Restart-VMGuest $s -Confirm:$False
+Get-VIEvent -Entity $s -MaxSamples 1 | select FullFormattedMessage, CreatedTime
+
+# Get task from cluster level
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PowerCLI
+# test1
